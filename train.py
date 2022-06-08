d9fb319cfdb2d208e4a2714ad87ae62d7c69a312
--global <--global>
2022-06-08 10:24:50 -0300

    latest experiments

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 from pideq.net import PINN
 from pideq.trainer import Trainer4T
@@ -8,23 +9,24 @@ from pideq.utils import load_from_wandb
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    T = 2.
+    T = 20.
 
     torch.autograd.set_detect_anomaly(True)
-    for _ in range(1):
-        net = PINN(T, n_nodes=100).to(device)
-        # net = load_from_wandb(net, '2f2dmyf0')
+    for _ in range(5):
+        net = PINN(T, n_nodes=20).to(device)
+        # net = load_from_wandb(net, '26wbor82', model_fname='model_best')
         Trainer4T(
             net,
             lr=1e-3,
-            epochs=1000,
+            epochs=100000,
             T=T,
             val_dt=.1,
+            lamb=.01,
             # optimizer='LBFGS',
             # wandb_project=None,
             wandb_group='test-kurz',
             # lr_scheduler='CosineAnnealingLR',
-            # lr_scheduler_params={'T_max': 200, 'eta_min': 1e-3},
+            # lr_scheduler_params={'T_max': 200, 'eta_min': 1e-4},
             mixed_precision=False,
             # loss_func='L1Loss',
         ).run()
