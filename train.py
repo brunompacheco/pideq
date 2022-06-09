3b96e0aef2354617e7af54e22c88dc07343959a1
--global <--global>
2022-06-09 14:41:26 -0300

    experiments with L-BFGS

--- train.py
+++ train.py
@@ -10,24 +10,25 @@ from pideq.utils import load_from_wandb
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    T = 10.
+    T = 2.
 
     # torch.autograd.set_detect_anomaly(True)
     for _ in range(1):
         net = PINN(T, n_out=2, n_hidden=4, n_nodes=20).to(device)
-        # net = load_from_wandb(net, '1mez9f3f')
+        net = load_from_wandb(net, 'o76oyh72')
         Trainer4T(
             net,
-            lr=1e-2,
-            epochs=1000000,
+            # lr=1e-3,
+            lr=1,
+            epochs=1,
             T=T,
-            val_dt=.01,
-            # optimizer='LBFGS',
-            # optimizer_params={'max_iter': 1, 'max_eval': 2000},
+            val_dt=.1,
+            optimizer='LBFGS',
+            optimizer_params={'max_iter': 2000,},
             # wandb_project=None,
             wandb_group='test-vdp',
             # lr_scheduler='CosineAnnealingLR',
             # lr_scheduler_params={'T_max': 200, 'eta_min': 1e-3},
-            mixed_precision=True,
+            mixed_precision=False,
             # loss_func='L1Loss',
         ).run()
