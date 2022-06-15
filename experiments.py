15832bf26fff40cf9b1cb358fe6ede8a3dca921d
--global <--global>
2022-06-14 21:54:27 -0300

    FCN experiments for VdP

--- experiments.py
+++ experiments.py
@@ -9,13 +9,11 @@ if __name__ == '__main__':
 
     # Experiment 1
     # Baseline FCN for different time horizons
-    for T in [10, 100, 1000]:
-        epochs = 10000 if T == 10 else 100000
-
+    for T, epochs in [(2, 1e4), (5, 1e5), (10, 3e5)]:
         for _ in range(5):
             Trainer4T(
                 PINN(T, n_nodes=20).to(device),
-                lr=1e-3,
+                lr=1e-3 if T <= 5 else 1e-2,
                 epochs=epochs,
                 T=T,
                 val_dt=T/1000,
