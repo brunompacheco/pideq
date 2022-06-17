944e617c0dae0d8a6e18943595c9cffb5a66591a
--global <--global>
2022-06-17 09:02:40 -0300

    latest experiments

--- train.py
+++ train.py
@@ -13,22 +13,64 @@ if __name__ == '__main__':
     T = 2.
 
     # torch.autograd.set_detect_anomaly(True)
+    # TODO: check if jac_loss_t is overwhelming the gradients in comparison to jac_loss_f
     for _ in range(1):
-        net = PINN(T, n_out=2, n_hidden=4, n_nodes=20).to(device)
-        net = load_from_wandb(net, 'o76oyh72')
-        Trainer4T(
-            net,
-            # lr=1e-3,
-            lr=1,
-            epochs=1,
-            T=T,
-            val_dt=.1,
-            optimizer='LBFGS',
-            optimizer_params={'max_iter': 2000,},
-            # wandb_project=None,
-            wandb_group='test-vdp',
-            # lr_scheduler='CosineAnnealingLR',
-            # lr_scheduler_params={'T_max': 200, 'eta_min': 1e-3},
-            mixed_precision=False,
-            # loss_func='L1Loss',
-        ).run()
+        try:
+            DEQTrainer4T(
+                PIDEQ(T, n_states=80).to(device),
+                lr=1e-3,
+                epochs=50000,
+                jac_lamb=.01,
+                T=T,
+                val_dt=.1,
+                # wandb_project=None,
+                wandb_group='test-deq',
+                mixed_precision=False,
+            ).run()
+        except Exception as e:
+            print(e)
+        try:
+            DEQTrainer4T(
+                PIDEQ(T).to(device),
+                lr=1e-4,
+                epochs=50000,
+                jac_lamb=.1,
+                T=T,
+                val_dt=.1,
+                # wandb_project=None,
+                wandb_group='test-deq',
+                mixed_precision=False,
+            ).run()
+        except Exception as e:
+            print(e)
+        try:
+            DEQTrainer4T(
+                PIDEQ(T, n_states=80).to(device),
+                lr=1e-4,
+                epochs=50000,
+                jac_lamb=.01,
+                T=T,
+                val_dt=.1,
+                # wandb_project=None,
+                wandb_group='test-deq',
+                mixed_precision=False,
+            ).run()
+        except Exception as e:
+            print(e)
+        # Trainer4T(
+        #     net,
+        #     lr=1e-3,
+        #     epochs=100000,
+        #     T=T,
+        #     val_dt=.1,
+        #     # optimizer='LBFGS',
+        #     # optimizer_params={'max_iter': 2000,},
+        #     # wandb_project=None,
+        #     wandb_group='test',
+        #     lr_scheduler='MultiStepLR',
+        #     lr_scheduler_params={'milestones': [60000, 80000]}
+        #     # lr_scheduler='CosineAnnealingLR',
+        #     # lr_scheduler_params={'T_max': 200, 'eta_min': 1e-3},
+        #     mixed_precision=False,
+        #     # loss_func='L1Loss',
+        # ).run()
