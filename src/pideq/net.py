1492cb918163704bdf4621672789069110b8da96
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2022-09-19 10:28:38 -0300

    refactor physics-informed class out of PINN and PIDEQ

--- src/pideq/net.py
+++ src/pideq/net.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from turtle import forward
+
 import numpy as np
 import torch
 import torch.nn as nn
@@ -6,12 +9,31 @@ from pideq.deq.model import DEQ
 from pideq.deq.solvers import anderson, forward_iteration
 
 
-class PINN(nn.Module):
-    def __init__(self, T: float, n_in=2, n_out=2, Nonlin=nn.Tanh,
-                 n_hidden=4, n_nodes=100, xb=[-5, 5]) -> None:
+class PhysicsInformedModel(nn.Module,ABC):
+    def __init__(self, T: float) -> None:
         super().__init__()
 
         self.T = T
+    
+    @abstractmethod
+    def _forward(self, x):
+        """Run inner model on (already stacked and normalized) input.
+        """
+    
+    def forward(self, t, x):
+        # rescaling the input => better convergence
+        # y = self.fcn(t / self.T) + self.eps
+        x_ = torch.hstack((
+            2 * t / self.T - 1,
+            2 * (x - self.xb[0]) / (self.xb[1] - self.xb[0]) - 1
+        ))
+
+        return self._forward(x_)
+
+class PINN(PhysicsInformedModel):
+    def __init__(self, T: float, n_in=2, n_out=2, Nonlin=nn.Tanh,
+                 n_hidden=4, n_nodes=100, xb=[-5, 5]) -> None:
+        super().__init__(T)
 
         self.xb = np.array(xb)
 
@@ -35,23 +57,8 @@ class PINN(nn.Module):
                 m.bias.data.fill_(0.01)
         self.fcn.apply(init_weights)
 
-        # self.eps = 1e-3  # added to the output, avoids sqrt gradient at 0
-
-        # assert y0.shape[0] == n_out
-
-        # self.y0 = torch.Tensor(y0)
-
-    def forward(self, t, x):
-        # rescaling the input => better convergence
-        # y = self.fcn(t / self.T) + self.eps
-        x_ = torch.hstack((
-            2 * t / self.T - 1,
-            2 * (x - self.xb[0]) / (self.xb[1] - self.xb[0]) - 1
-        ))
-
-        h = self.fcn(x_)
-
-        return h
+    def _forward(self, x_):
+        return self.fcn(x_)
 
 class PINC(PINN):
     def __init__(self, T: float, n_out=2, y_bounds=np.array([[-5, 5], [-5, 5]]),
@@ -74,16 +81,18 @@ class PINC(PINN):
 
         return y * y_range.to(y) + self.y0.to(y)
 
-class PIDEQ(DEQ):
+class PIDEQ(PhysicsInformedModel,DEQ):
     def __init__(self, T: float, n_in=2, n_out=2, n_states=100, n_hidden=1,
                  nonlin=torch.tanh, always_compute_grad=False, solver=anderson,
                  solver_kwargs={'threshold': 200, 'eps':1e-4}, xb=[-5, 5],
                 ) -> None:
-        super().__init__(n_in, n_out, n_states, n_hidden, nonlin, always_compute_grad, solver, solver_kwargs)
-
-        self.T = T
+        PhysicsInformedModel.__init__(self,T)
+        DEQ.__init__(self, n_in, n_out, n_states, n_hidden, nonlin, always_compute_grad, solver, solver_kwargs)
 
         self.xb = np.array(xb)
+
+    def _forward(self, x_):
+        return DEQ.forward(self, x_)
 
     @classmethod
     def from_pinn(cls, pinn: PINN):
@@ -93,8 +102,8 @@ class PIDEQ(DEQ):
             if isinstance(l, nn.Linear):
                 n_states += l.out_features
 
-        deq = cls(
-            pinn.T,
+        self = cls(
+            T=pinn.T,
             n_in=pinn.fcn[0].in_features,
             n_out=pinn.fcn[-1].out_features,
             n_states=n_states,
@@ -106,17 +115,17 @@ class PIDEQ(DEQ):
 
         l1 = pinn.fcn[0]  # first layer
 
-        A_w = torch.zeros_like(deq.A.weight)
+        A_w = torch.zeros_like(self.A.weight)
         A_w[:l1.weight.shape[0]] = l1.weight
-        deq.A.weight = nn.Parameter(A_w)
+        self.A.weight = nn.Parameter(A_w)
 
-        A_b = torch.zeros_like(deq.A.bias)
+        A_b = torch.zeros_like(self.A.bias)
         A_b[:l1.bias.shape[0]] = l1.bias
-        deq.A.bias = nn.Parameter(A_b)
+        self.A.bias = nn.Parameter(A_b)
 
         # build B matrix from hidden layers
-        B_w = torch.zeros_like(deq.B.weight)
-        B_b = torch.zeros_like(deq.B.bias)
+        B_w = torch.zeros_like(self.B.weight)
+        B_b = torch.zeros_like(self.B.bias)
 
         l0 = pinn.fcn[0].out_features  # end of the last hidden layer's output
         for l in pinn.fcn[1:-2]:  # skip first and last layers
@@ -125,26 +134,18 @@ class PIDEQ(DEQ):
                 B_b[l0:l0 + l.out_features] = l.bias
                 l0 = l0 + l.out_features
 
-        deq.B.weight = nn.Parameter(B_w)
-        deq.B.bias = nn.Parameter(B_b)
+        self.B.weight = nn.Parameter(B_w)
+        self.B.bias = nn.Parameter(B_b)
 
         # build h function from last layer
         ll = pinn.fcn[-1]  # last layer
 
-        h_w = torch.zeros_like(deq.h.weight)
+        h_w = torch.zeros_like(self.h.weight)
         h_w[:,-ll.weight.shape[-1]:] = ll.weight
-        deq.h.weight = nn.Parameter(h_w)
+        self.h.weight = nn.Parameter(h_w)
 
-        h_b = torch.zeros_like(deq.h.bias)
+        h_b = torch.zeros_like(self.h.bias)
         h_b[-ll.bias.shape[0]:] = ll.bias
-        deq.h.bias = nn.Parameter(h_b)
+        self.h.bias = nn.Parameter(h_b)
 
-        return deq
-
-    def forward(self, t, x):
-        x_ = torch.hstack((
-            2 * t / self.T - 1,
-            2 * (x - self.xb[0]) / (self.xb[1] - self.xb[0]) - 1
-        ))
-
-        return super().forward(x_)
+        return self
