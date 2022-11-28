40f700757f7d389b004afa5e2597ca21c9a60df6
--global <--global>
2022-11-28 18:20:24 -0300

    refactor get_jacobian from trainer

--- src/pideq/utils.py
+++ src/pideq/utils.py
@@ -5,6 +5,8 @@ import wandb
 import torch
 import torch.nn as nn
 
+from torch.autograd import grad
+
 
 def load_from_wandb(net: nn.Module, run_id: str,
                     project='pideq-sine', model_fname='model_last'):
@@ -24,3 +26,17 @@ def debugger_is_active() -> bool:
     """
     gettrace = getattr(sys, 'gettrace', lambda : None) 
     return gettrace() is not None
+
+def get_jacobian(Y, x_):
+    """Compute the jacobian of a vector->vector function using `autograd.grad`.
+    """
+    grads_y = list()
+    for i in range(Y.shape[1]):
+        grad_y = grad(Y[:,i].sum(), x_, create_graph=True)[0]
+        grad_y = grad_y.unsqueeze(1)  # effectively transposing each vector of the batch
+
+        grads_y.append(grad_y)
+
+    J = torch.cat(grads_y, dim=1)
+
+    return J
